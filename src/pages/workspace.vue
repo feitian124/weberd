086bc4b8ae9470e529dcc408d7be70db6a584afe
feitian124 <feitian124@gmail.com>
2021-08-22 16:40:03 +0800

    add workspace page, x6 works

---
+++ src/pages/workspace.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { Graph } from '@antv/x6'
+
+const data = {
+  // 节点
+  nodes: [
+    {
+      id: 'node1', // String，可选，节点的唯一标识
+      x: 40, // Number，必选，节点位置的 x 值
+      y: 40, // Number，必选，节点位置的 y 值
+      width: 80, // Number，可选，节点大小的 width 值
+      height: 40, // Number，可选，节点大小的 height 值
+      label: 'hello', // String，节点标签
+    },
+    {
+      id: 'node2', // String，节点的唯一标识
+      x: 160, // Number，必选，节点位置的 x 值
+      y: 180, // Number，必选，节点位置的 y 值
+      width: 80, // Number，可选，节点大小的 width 值
+      height: 40, // Number，可选，节点大小的 height 值
+      label: 'world', // String，节点标签
+    },
+  ],
+  // 边
+  edges: [
+    {
+      source: 'node1', // String，必须，起始节点 id
+      target: 'node2', // String，必须，目标节点 id
+    },
+  ],
+}
+
+onMounted(() => {
+  const graph = new Graph({
+    container: document.getElementById('container'),
+    background: {
+      color: '#fffbe6', // 设置画布背景颜色
+    },
+    grid: {
+      size: 10, // 网格大小 10px
+      visible: true, // 渲染网格背景
+    },
+  })
+
+  graph.fromJSON(data)
+})
+</script>
+
+<template>
+  <div id="container" class="h-full"></div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: workspace
+</route>
